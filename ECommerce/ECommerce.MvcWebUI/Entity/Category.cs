dc32fc0beafc459e7bb20011be9a888d8a8ee142
Ali Bulut <alibulut@yahoo.com>
2019-09-02 13:51:11 +0300

    admin category panel was added

--- ECommerce/ECommerce.MvcWebUI/Entity/Category.cs
+++ ECommerce/ECommerce.MvcWebUI/Entity/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,9 @@ namespace ECommerce.MvcWebUI.Entity
     public class Category
     {
         public int Id { get; set; }
+
+        //Buraya Kategori Adı yazarsan vs'nin kendi oluşturduğu sayfalarda Name yerine Kategori Adı gösterilir
+        //[DisplayName("Kategori Adı")]
         public string Name { get; set; }
         public string Description { get; set; }
 
