06882027bffcd20530f42be5b68e0327efe38956
Ali Bulut <alibulut@yahoo.com>
2019-09-04 12:48:16 +0300

    shopping cart added

---
+++ ECommerce/ECommerce.MvcWebUI/Controllers/CartController.cs
@@ -0,0 +1,58 @@
+﻿using ECommerce.MvcWebUI.Entity;
+using ECommerce.MvcWebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerce.MvcWebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private DataContext db = new DataContext();
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View(GetCart());
+        }
+        public ActionResult AddToCart(int Id)
+        {
+            var product = db.Products.Where(i => i.Id == Id).FirstOrDefault();
+            if (product!=null)
+            {
+                GetCart().AddProduct(product, 1);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult RemoveFromCart(int Id)
+        {
+            var product = db.Products.Where(i => i.Id == Id).FirstOrDefault();
+            if (product != null)
+            {
+                GetCart().DeleteProduct(product);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public Cart GetCart()
+        {
+            //session her kullanıcıya özel bir depo
+            Cart cart = (Cart)Session["Cart"];
+            if (cart==null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+
+            return cart;
+        }
+        
+        public PartialViewResult Summary()
+        {
+            return PartialView(GetCart());
+        }
+    }
+}
